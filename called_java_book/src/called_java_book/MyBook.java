f8edf95ba1b80b4836ab2210e00c0b547a15814d
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-04-13 17:52:52 +0900

    자바 클래스 만들기 연습

---
+++ called_java_book/src/called_java_book/MyBook.java
@@ -0,0 +1,18 @@
+package called_java_book;
+
+public class MyBook {
+
+	public static void main(String[] args) {
+	
+		JavaBook artOfComputerProgramming = new JavaBook();
+		JavaBook EffectiveJava = new JavaBook();
+		JavaBook cleanCode = new JavaBook();
+		
+		
+		artOfComputerProgramming.MyBook();
+		EffectiveJava.MyBook();
+		cleanCode.MyBook();
+
+	}
+
+}
