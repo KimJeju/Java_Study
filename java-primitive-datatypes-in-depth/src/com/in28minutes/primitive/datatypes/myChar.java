8564b1eef10b059535f1aafc407aa506ba57eb18
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-04-19 18:05:23 +0900

    char 형 예제 풀이중

--- java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myChar.java
+++ java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myChar.java
@@ -1,5 +1,13 @@
 package com.in28minutes.primitive.datatypes;
 
+// ´ë¹®ÀÚ 65 ~ 90
+
 public class myChar {
+
+  char ch = 'c';
+  
+  myChar(char ch) {
+	  
+  }
 
 }
--- java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
+++ java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
@@ -3,7 +3,14 @@ package com.in28minutes.primitive.datatypes;
 public class myCharRunner {
 
 	public static void main(String[] args) {
-	  myChar myChar = new mychar()
+	  myChar myChar = new myChar();
+	  System.out.println(myChar.isVowel());
+	  
+	  System.out.println(myChar.isNumber());
+	  System.out.println(myChar.isAlphabet());
+	  
+	  myChar.printLowerCaseAlphabets();
+	  myChar.printUpperCaseAlphabets();
 	}
 
 }
