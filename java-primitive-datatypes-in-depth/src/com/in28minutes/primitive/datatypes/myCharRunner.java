02e292d50972a90c2ab3a4c214384b186dfc2cad
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-04-20 18:08:25 +0900

    backUp.txt

--- java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
+++ java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
@@ -3,15 +3,18 @@ package com.in28minutes.primitive.datatypes;
 public class myCharRunner {
 
 	public static void main(String[] args) {
+		
 	  myChar myChar = new myChar('c');
 	  System.out.println(myChar.isVowel());
 	  
+	  
+	  // myChar에서 받은 레터럴값이 숫자인지 알파벳인지 boolean 값으로 출력
 	  System.out.println(myChar.isNumber());
 	  System.out.println(myChar.isAlphabet());
 	  
-	  
+	  // 반환되는 레터럴값이 소문자거나 대문자일 경우 엮으로 바꿔주고 출력
 	  System.out.println("");
-	  //myChar.printLowerCaseAlphabets();
+	  myChar.printLowerCaseAlphabets();
 	  myChar.printUpperCaseAlphabets();
 	}
 
