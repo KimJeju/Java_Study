de042f09eca896c50113afb13f592412f96251a3
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-04-20 10:12:53 +0900

    isNumber 푸는중

--- java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myChar.java
+++ java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myChar.java
@@ -4,10 +4,34 @@ package com.in28minutes.primitive.datatypes;
 
 public class myChar {
 
-  char ch = 'c';
-  
-  myChar(char ch) {
-	  
-  }
+	private char ch;
+	
+	public myChar(char ch) {
+		this.ch = ch;
+	}
+	
+	
+	public boolean isVowel() {
+		// a e i o u or A E I O U
+		if(ch == 'a' || ch == 'e' || ch == 'i' ||ch ==  'o' ||ch ==  'u' ||
+				ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+		return true;
+		
+		else
+		return false;
+	}
+	
+
+	public void isNumber(char isNum) {
+		if(this.ch == isNum) {
+			for(char i = 48; i <= 57; ++i ) {
+			System.out.println(true);	
+			}	
+		}
+		
+		else
+		System.out.println(false);
+	}
+
 
 }
--- java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
+++ java-primitive-datatypes-in-depth/src/com/in28minutes/primitive/datatypes/myCharRunner.java
@@ -3,14 +3,14 @@ package com.in28minutes.primitive.datatypes;
 public class myCharRunner {
 
 	public static void main(String[] args) {
-	  myChar myChar = new myChar();
+	  myChar myChar = new myChar('a');
 	  System.out.println(myChar.isVowel());
 	  
 	  System.out.println(myChar.isNumber());
-	  System.out.println(myChar.isAlphabet());
+	//  System.out.println(myChar.isAlphabet());
 	  
-	  myChar.printLowerCaseAlphabets();
-	  myChar.printUpperCaseAlphabets();
+	//  myChar.printLowerCaseAlphabets();
+	//  myChar.printUpperCaseAlphabets();
 	}
 
 }
