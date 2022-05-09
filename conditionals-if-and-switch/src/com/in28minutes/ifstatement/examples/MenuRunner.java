8d63bf2abb8c24912c35d73879668882862b3aae
KimJeju <Kingswingsex@gmail.com>
2022-05-09 17:39:01 +0900

    backUptxt

--- conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/MenuRunner.java
+++ conditionals-if-and-switch/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -2,36 +2,19 @@ package com.in28minutes.ifstatement.examples;
 
 //사용자 입력값을 받아 그에 맞는 함수를 호출해주는 러너
 
-import java.util.Scanner;  // 컨트롤 + 1 을 눌러 임포트 할 수 있다. 
+import java.util.Scanner;
                            // 스캐너는 자바 내장 클래스 이기 때문에 임포트를 해야한다.
 public class MenuRunner {
 
 	public static void main(String[] args) {
 		
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("좆같다 씨발 무슨 욕할래?");
+			
+			String Sibal = scanner.nextLine();
 
-		Scanner scanner = new Scanner(System.in); //인스턴트 객체 선언 
-		
-		System.out.println("Enter Number1 : ");
-		int Number1 = scanner.nextInt();  //사용자 입력값을 받아준다.
-		
-		System.out.println("The Number you Entered is = " + Number1); // 문자열 뒤에 + 를 붙여주면 정수를 붙여서 출력해준다.
-		
-		System.out.println("Enter Number2 :");
-		int Number2 = scanner.nextInt();
-		
-		System.out.println("The Number you Enter ed is = " + Number2);
-		
-		System.out.println("Enter the Menu" + '\n' + "1.add" + '\n' + "2.Multyply");
-		int Menu = scanner.nextInt();
-		
-		if(Menu == 1) {
-			int add = Number1 + Number2;
-			System.out.println("add = " + add);
-		}else if(Menu == 2) {
-			int Multiply = Number1 * Number2;
-			System.out.println("Multlply =" + Multiply);
-		}
-		
+			System.out.println(Sibal);
 			
 
 	}
