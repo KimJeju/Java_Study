78e5c873cc5b7e2dd9ce05bfe6630fbb4b33f8eb
KimJeju <kingswingsex@gmail.com>
2022-05-11 20:53:33 +0900

    생성자테스트

---
+++ IntelliJ/java_learn/src/carTest.java
@@ -0,0 +1,50 @@
+public class carTest {
+    public static void main(String[] args) {
+        Car bmw = new Car("red");
+        Car jeep = new Car("jeep","black",130);
+        System.out.println(bmw.convertible(true));
+        System.out.println();
+    }
+}
+
+class Car{
+
+    private String model;
+    private String color;
+    private int MaxSpeed;
+
+    Car(){
+        System.out.println("기본 생성자");
+    }
+
+    Car(String model){
+        this(model,"red",150);
+    }
+
+    Car(String model, String color,int maxSpeed){
+        this.model = model;
+        this.color = color;
+        this.MaxSpeed = maxSpeed;
+    }
+
+    void start() {
+        System.out.println("시동 걸렸다 부릉부릉");
+    }
+
+    void upSpeed() {
+        System.out.println("시동 건다 얍");
+    }
+
+    boolean convertible(boolean Convertible){
+
+        if(Convertible == true){
+            System.out.println("오픈카");
+        }else {
+            System.out.println("그냥 차");
+        }
+
+        return Convertible;
+    }
+
+}
+
