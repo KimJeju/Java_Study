542c81168a79cce20d2e32d471abc4375ecadff5
KimJeju <Kingswingsex@gmail.com>
2022-05-24 19:06:28 +0900

    학생 성적관리 프로그램 스스로 작성해보기

--- java_28minutes/src/studentTest.java
+++ java_28minutes/src/studentTest.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.*;
 
@@ -17,6 +18,9 @@ public class studentTest {
 
         int NumMin = student.getMinimumMark();
         System.out.println(NumMin);
+
+        BigDecimal average = student.getAverage();
+        System.out.println(average);
 
     }
 }
@@ -45,37 +49,39 @@ class Student{
     }
 
     int getMaxiMumMark(){
-        int max = marks[0];
+        int max = 0;
 
-        for(int i = 0; i <= marks.length - 1; ++i){
-            if(max < marks[i]){
-                max = marks[i];
+        for(int mark:marks){
+            if(max<mark){
+                max = mark;
             }
         }
+
+//        for(int i = 0; i <= marks.length - 1; ++i){
+//            if(max < marks[i]){
+//                max = marks[i];
+//            }
+//        }
 
         return max;
     }
 
     int getMinimumMark(){
-        int min = marks[0];
+        int min = Integer.MAX_VALUE;
 
-        for(int i = 0; i <= marks.length - 1; ++i){
-            if(min > marks[i]){
-                min = marks[i];
+        for(int mark:marks){ //99,100,95,92
+            if(mark<min){
+                min = mark;
             }
         }
         return min;
-
     }
 
-//    double getAverage(){
-//        BigDecimal avg = new BigDecimal(0);
-//
-//        for(int i = 0; i < marks.length; ++i){
-//            avg.divide(()marks[i])
-//        }
-//    }
 
+    public BigDecimal getAverage() {
+        int sum = getToTotalSumMarks();
+        int number = getNumberOfmarks();
 
-
+        return new BigDecimal(sum).divide(new BigDecimal(number),3, RoundingMode.UP); //빅데시말 소수 3자리 까지 표현 및 4자리 반올림
+    }
 }
