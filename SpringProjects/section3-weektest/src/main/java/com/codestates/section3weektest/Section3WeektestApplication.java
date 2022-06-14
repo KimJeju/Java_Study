19d8cc4f361aa8eebaa03b6dd5cf152b05c344c3
KimJeju <kingswingsex@gmail.com>
2022-06-14 11:09:25 +0900

    스프링환경 맥북테스트

---
+++ SpringProjects/section3-weektest/src/main/java/com/codestates/section3weektest/Section3WeektestApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.section3weektest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3WeektestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3WeektestApplication.class, args);
+	}
+
+}
---
+++ SpringProjects/section3-weektest/src/test/java/com/codestates/section3weektest/Section3WeektestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.section3weektest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section3WeektestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
