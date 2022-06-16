cb75730c307b9ef6ac2f2796800ad1c736c44605
KimJeju <Kingswingsex@gmail.com>
2022-06-16 18:12:12 +0900

    싱글톤 패턴과 싱글톤 컨테이너 사용해보기

--- SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/AppConfig.java
+++ SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/AppConfig.java
@@ -9,20 +9,28 @@ import com.codestates.code.test.SpringBoot.user.UserRepositoryImpl;
 import com.codestates.code.test.SpringBoot.user.UserRepository;
 import com.codestates.code.test.SpringBoot.user.UserService;
 import com.codestates.code.test.SpringBoot.user.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public UserService userService(){
         return new UserServiceImpl(UserRepository());
     }
 
+    @Bean
     public UserRepository UserRepository(){
         return new UserRepositoryImpl();
     }
+
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(UserRepository(),discountInfo());
     }
 
+    @Bean
     public DiscountInfo discountInfo(){
         //return new CurrentDiscountInfo();
         return new RateDiscountInfo();
