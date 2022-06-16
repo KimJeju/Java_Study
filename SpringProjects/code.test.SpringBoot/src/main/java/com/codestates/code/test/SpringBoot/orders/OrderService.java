8cfaed3d5aeac7668a86e184c9ff8b93b9fbaffe
KimJeju <Kingswingsex@gmail.com>
2022-06-16 11:32:17 +0900

    주문 로직

---
+++ SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/orders/OrderService.java
@@ -0,0 +1,5 @@
+package com.codestates.code.test.SpringBoot.orders;
+
+public interface OrderService {
+    Order createOrder(Long userId, String iTemName, String iTemPrice);
+}
---
+++ SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/orders/OrderServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codestates.code.test.SpringBoot.orders;
+import com.codestates.code.test.SpringBoot.orders.Order;
+import com.codestates.code.test.SpringBoot.orders.OrderService;
+
+public class OrderServiceImpl {
+}
