56692e9afc215fa280dec58f2c81800a86c8d3c6
KimJeju <Kingswingsex@gmail.com>
2022-06-16 13:49:30 +0900

    최종수정

--- SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/AppConfig.java
+++ SpringProjects/code.test.SpringBoot/src/main/java/com/codestates/code/test/SpringBoot/AppConfig.java
@@ -1,8 +1,10 @@
 package com.codestates.code.test.SpringBoot;
 
+import com.codestates.code.test.SpringBoot.discount.RateDiscountInfo;
 import com.codestates.code.test.SpringBoot.orders.OrderService;
 import com.codestates.code.test.SpringBoot.orders.OrderServiceImpl;
 import com.codestates.code.test.SpringBoot.discount.CurrentDiscountInfo;
+import com.codestates.code.test.SpringBoot.discount.DiscountInfo;
 import com.codestates.code.test.SpringBoot.user.UserRepositoryImpl;
 import com.codestates.code.test.SpringBoot.user.UserRepository;
 import com.codestates.code.test.SpringBoot.user.UserService;
@@ -11,11 +13,19 @@ import com.codestates.code.test.SpringBoot.user.UserServiceImpl;
 public class AppConfig {
 
     public UserService userService(){
-        return new UserServiceImpl(new UserRepositoryImpl());
+        return new UserServiceImpl(UserRepository());
     }
 
+    public UserRepository UserRepository(){
+        return new UserRepositoryImpl();
+    }
     public OrderService orderService(){
-        return new OrderServiceImpl(new UserRepositoryImpl(),new CurrentDiscountInfo());
+        return new OrderServiceImpl(UserRepository(),discountInfo());
+    }
+
+    public DiscountInfo discountInfo(){
+        //return new CurrentDiscountInfo();
+        return new RateDiscountInfo();
     }
 
 }
