3b508e4709e692fa1d289071434a552569fa3fc1
KimJeju <Kingswingsex@gmail.com>
2022-06-17 18:46:41 +0900

    backUp Spring

--- SpringProjects/hello-spring/src/main/java/hello/hellospring/contlroller/HelloController.java
+++ SpringProjects/hello-spring/src/main/java/hello/hellospring/contlroller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,30 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloSpring(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setHello(name);
+        return hello;
+    }
+
+
+    static class Hello{
+        private String hello;
+        public String getHello(){
+            return hello;
+        }
+        public void setHello(String hello){
+            this.hello = hello;
+        }
+    }
+
 }
