c5cd14534b8e1dd6dfa4be941798923068fc776d
KimJeju <Kingswingsex@gmail.com>
2022-06-27 19:57:40 +0900

    김영한교수님 강의 혼자 만들어보기

---
+++ SpringProjects/coreSelf/src/main/java/helloSelf/coreSelf/CoreSelfApplication.java
@@ -0,0 +1,13 @@
+package helloSelf.coreSelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSelfApplication.class, args);
+	}
+
+}
