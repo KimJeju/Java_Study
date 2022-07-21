91faa325530b4cad9c570b511da55a28ac80f6ab
KimJeju <Kingswingsex@gmail.com>
2022-07-21 17:30:30 +0900

    아..

---
+++ SpringProjects/httpServer/src/main/java/com/httpServerTest/httpServer/HttpServerApplication.java
@@ -0,0 +1,13 @@
+package com.httpServerTest.httpServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpServerApplication.class, args);
+	}
+
+}
